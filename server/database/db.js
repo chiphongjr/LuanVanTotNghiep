85e4fcc4fbe09c1fd354ebca40f60fa8609b7894
chiphongjr <ckjfong204@gmail.com>
2026-01-01 02:55:13 +0700

    new db chay deploy

--- server/database/db.js
+++ server/database/db.js
@@ -1,13 +1,36 @@
+// import pkg from "pg";
+
+// const { Client } = pkg;
+
+// const database = new Client({
+//   user: "postgres",
+//   host: "localhost",
+//   database: process.env.DB_NAME,
+//   password: "Phong20042003",
+//   port: 5432,
+// });
+
+// try {
+//   await database.connect();
+//   console.log("Connect db successfully");
+// } catch (error) {
+//   console.error("Connect db fail", error);
+//   process.exit(1);
+// }
+// export default database;
+
+
 import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const { Client } = pkg;
 
 const database = new Client({
-  user: "postgres",
-  host: "localhost",
-  database: process.env.DB_NAME,
-  password: "Phong20042003",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // cần khi kết nối DB online
+  },
 });
 
 try {
@@ -17,4 +40,5 @@ try {
   console.error("Connect db fail", error);
   process.exit(1);
 }
+
 export default database;

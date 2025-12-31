e26129bce66a0ceca125174b9bfa5a4446cc61b5
chiphongjr <ckjfong204@gmail.com>
2026-01-01 03:00:19 +0700

    thay 2 link deploy vao

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    origin: ["https://admin-jrshop.vercel.app/login", "https://admin-jrshop.vercel.app/login"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })

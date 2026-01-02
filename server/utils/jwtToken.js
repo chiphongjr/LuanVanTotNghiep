245e28f32bd56e83674433bf323bbc5100255682
chiphongjr <ckjfong204@gmail.com>
2026-01-03 01:00:37 +0700

    token mobile not fix

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -1,30 +1,5 @@
 import jwt from "jsonwebtoken";
 
-// export const sendToken = (user, statusCode, message, res) => {
-//   const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, {
-//     expiresIn: process.env.JWT_EXPIRES_IN,
-//   });
-
-//   res
-//     .status(statusCode)
-//     .cookie("token", token, {
-//       expires: new Date(
-//         Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-//       ),
-//       httpOnly: true,
-//       sameSite: "none",
-//       secure: true,
-//     })
-//     .json({
-//       success: true,
-//       user,
-//       message,
-//       token,
-//     });
-// };
-
-// Thay đổi sameSite và secure cho môi trường Production (HTTPS)
-// Thay đổi tạm thời trong utils/jwtToken.js để kiểm tra
 export const sendToken = (user, statusCode, message, res) => {
   const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, {
     expiresIn: process.env.JWT_EXPIRES_IN,
@@ -37,7 +12,7 @@ export const sendToken = (user, statusCode, message, res) => {
         Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
-      sameSite: "None",
+      sameSite: "none",
       secure: true,
     })
     .json({

42d7fcde76b4a750e0a6ec0db4d6b4180caf6583
chiphongjr <ckjfong204@gmail.com>
2026-01-02 15:27:27 +0700

    thay db + config

--- server/database/db.js
+++ server/database/db.js
@@ -2,19 +2,28 @@ import pkg from "pg";
 
 const { Client } = pkg;
 
+// const database = new Client({
+//   user: "postgres",
+//   host: "localhost",
+//   database: process.env.DB_NAME,
+//   password: "Phong20042003",
+//   port: 5432,
+// });
+
+// try {
+//   await database.connect();
+//   console.log("Connect db successfully");
+// } catch (error) {
+//   console.error("Connect db fail", error);
+//   process.exit(1);
+// }
+
 const database = new Client({
-  user: "postgres",
-  host: "localhost",
-  database: process.env.DB_NAME,
-  password: "Phong20042003",
-  port: 5432,
+  connectionString: process.env.DB_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
-try {
-  await database.connect();
-  console.log("Connect db successfully");
-} catch (error) {
-  console.error("Connect db fail", error);
-  process.exit(1);
-}
 export default database;
